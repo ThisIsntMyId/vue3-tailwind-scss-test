230e628378fec8f18593253b7ed6f590bba765bf
Dishant Pal <dishantpal37@gmail.com>
2021-01-31 19:54:22 +0530

    counter completed with tailwind

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,22 +1,61 @@
 <template>
   <div class="bg-blue-100 p-10">
-    {{ count }}
-    <button @click="count++">inc</button>
-    <button @click="count--">dec</button>
+    <div class="m-2 text-4xl">
+      {{ count }}
+    </div>
+    <button
+      class="border border-blue-900 hover:bg-blue-900 hover:text-white rounded p-2 m-3"
+      @click="count++"
+    >
+      inc
+    </button>
+    <button
+      class="border border-blue-900 hover:bg-blue-900 hover:text-white rounded p-2 m-3"
+      @click="count--"
+    >
+      dec
+    </button>
+    <button
+      class="border border-blue-900 hover:bg-blue-900 hover:text-white rounded p-2 m-3"
+      @click="count = count * multiplier"
+    >
+      mul
+    </button>
+    <div class="m-3">
+      <input
+        type="text"
+        class="border p-2 text-black font-black"
+        :class="{ 'text-red-500': inputError }"
+        v-model="multiplier"
+      />
+      <p v-if="inputError" class="text-red-500 mt-2">
+        multiplier should be a number between 1-100
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, computed } from "vue";
 
 export default {
   setup() {
     const state = reactive({
-      count: 0
+      count: 0,
+      multiplier: 1
+    });
+
+    const inputError = computed(() => {
+      const validMultiplierInput =
+        /^\d+$/g.test(state.multiplier) &&
+        parseInt(state.multiplier) > 0 &&
+        parseInt(state.multiplier) < 100;
+      return !validMultiplierInput;
     });
 
     return {
-      ...toRefs(state)
+      ...toRefs(state),
+      inputError
     };
   }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Home - Counter</h1>
     <Counter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Counter from "@/views/Counter.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Counter
   }
 };
