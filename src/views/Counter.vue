a76994aa7631795ed5abbd25d825fcb615a9a044
Dishant Pal <dishantpal37@gmail.com>
2021-01-31 19:18:37 +0530

    prettier config and tailwind added

---
+++ src/views/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-blue-100 p-10">
+    {{ count }}
+    <button @click="count++">inc</button>
+    <button @click="count--">dec</button>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0
+    });
+
+    return {
+      ...toRefs(state)
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
